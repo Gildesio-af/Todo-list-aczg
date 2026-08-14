39ef19d5e2d5813ccc4b01f2047a16307d8860a2
Gildesio-af <gildesioaf11@gmail.com>
2026-08-14 14:10:49 -0300

    feat: add user DTOs and custom exception for validation errors

---
+++ src/main/java/zg/acelera/util/exception/UserInfoWrongException.java
@@ -0,0 +1,7 @@
+package zg.acelera.util.exception;
+
+public class UserInfoWrongException extends RuntimeException{
+    public UserInfoWrongException(String message) {
+        super(message);
+    }
+}

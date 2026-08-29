f09d25348b450f4e0da63621ec0c586254934ec9
Gildesio-af <gildesioaf11@gmail.com>
2026-08-28 21:22:40 -0300

    chore: add constants for task status, filters and priorities

---
+++ front-end/js/constants.js
@@ -0,0 +1,16 @@
+export const TASK_STATUS = {
+    TODO: 'TODO',
+    DOING: 'DOING',
+    DONE: 'DONE'
+};
+
+export const FILTER_TYPES = {
+    ALL: 'all',
+    IMPORTANT: 'important',
+    DONE: 'done'
+};
+
+export const PRIORITY = {
+    LABELS: { '5': 'Máxima', '4': 'Alta', '3': 'Média', '2': 'Baixa', '1': 'Mínima' },
+    COLORS: { '5': '#dc2626', '4': '#ea580c', '3': '#d97706', '2': '#2563eb', '1': '#6b7280' }
+};
